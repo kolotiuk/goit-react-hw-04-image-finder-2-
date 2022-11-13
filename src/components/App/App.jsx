cd876e885092c198760de9d9d6711bcef180a4ca
Ihor <ihorfront88@gmail.com>
2022-11-13 20:29:22 +0200

    refactoring code from classes components to hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,10 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import { toast } from 'react-toastify';
+
+import { getImagesApi } from 'services/api';
 
 import Searchbar from '../Searchbar/Searchbar';
 import ImageGallery from '../ImageGallery';
@@ -8,52 +12,109 @@ import ImageGallery from '../ImageGallery';
 import s from './App.module.scss';
 import Modal from 'components/Modal';
 
-export default class App extends Component {
-  state = {
-    query: '',
-    modalShow: false,
-    largeImage: null,
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [modalShow, setModalShow] = useState(false);
+  const [largeImage, setLargeImage] = useState(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(0);
+  const [searchImageText, setSearchImageText] = useState('Search image');
+
+  useEffect(() => {
+    if (query !== '') {
+      setIsLoading(true);
+      setSearchImageText('');
+      setError(null);
+      setTotalHits(0);
+
+      getImagesApi({ q: query, page: page })
+        .then(data => {
+          if (!data.hits.length) {
+            toast.warn('No images by your request!');
+            return;
+          }
+          if (data.hits.length < 12) {
+            toast.warn('Its all images for your request!');
+          }
+          setImages([...images, ...data.hits]);
+          setTotalHits(data.totalHits);
+        })
+        .catch(error => {
+          setError(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, query]);
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+    setIsLoading(true);
+    handleWindowScrollBy();
   };
 
-  handleSubmit = query => {
-    this.setState({ query });
+  const handleWindowScrollBy = () => {
+    setTimeout(() => {
+      window.scrollBy({
+        top: window.innerHeight - 72 + 16,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }, 600);
   };
 
-  toggleModal = (largeImage = null) => {
-    this.setState(prev => ({
-      modalShow: !prev.modalShow,
-      largeImage: largeImage,
-    }));
+  const handleSubmit = query => {
+    setQuery(prev => {
+      if (prev !== query) {
+        setImages([]);
+        setPage(1);
+      }
+    });
+    setQuery(query);
   };
 
-  render() {
-    const { handleSubmit, toggleModal } = this;
-    const { query, modalShow, largeImage } = this.state;
-    return (
-      <div className={s.App}>
-        <Searchbar onSubmit={handleSubmit} />
-        <ImageGallery
-          query={query}
-          toggleModal={toggleModal}
-          modalShow={modalShow}
-        />
-        {modalShow && (
-          <Modal largeImage={largeImage} closeModal={toggleModal} />
-        )}
+  const toggleModal = (largeImageClick = null) => {
+    setModalShow(!modalShow);
+    setLargeImage(largeImageClick);
+  };
 
-        <ToastContainer
-          position="top-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={s.App}>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery
+        handleLoadMore={handleLoadMore}
+        totalHits={totalHits}
+        searchImageText={searchImageText}
+        error={error}
+        isLoading={isLoading}
+        images={images}
+        query={query}
+        toggleModal={toggleModal}
+        modalShow={modalShow}
+      />
+      {modalShow && <Modal largeImage={largeImage} closeModal={toggleModal} />}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+};
+
+export default App;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,82 +1,21 @@
 import { Component } from 'react';
 import s from './ImageGallery.module.scss';
-import { getImagesApi } from 'services/api';
 import ImageGalleryItem from 'components/ImageGalleryItem';
 import Button from 'components/Button';
 
-import { toast } from 'react-toastify';
 import Loader from 'components/Loader';
 
 class ImageGallery extends Component {
-  state = {
-    images: [],
-    page: 1,
-    isLoading: false,
-    error: null,
-    totalHits: 0,
-    searchImageText: 'Search image',
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const { query } = this.props;
-    const { page } = this.state;
-
-    if (prevProps.query !== query) {
-      this.setState({ images: [], page: 1 });
-    }
-
-    if (prevState.page !== page || (prevProps.query !== query && page === 1)) {
-      this.getImages();
-    }
-  }
-
-  getImages = () => {
-    this.setState({
-      isLoading: true,
-      searchImageText: '',
-      error: null,
-      totalHits: 0,
-    });
-    getImagesApi({ q: this.props.query, page: this.state.page })
-      .then(data => {
-        if (!data.hits.length) {
-          toast.warn('No images by your request!');
-        }
-        if (data.hits.length < 12) {
-          toast.warn('Its all images for your request!');
-        }
-        this.setState(prevState => ({
-          images: [...prevState.images, ...data.hits],
-          totalHits: data.totalHits,
-        }));
-      })
-      .catch(error => {
-        this.setState({ error });
-      })
-      .finally(() => {
-        this.setState({ isLoading: false });
-      });
-  };
-
-  handleLoadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1, isLoading: true }));
-    this.handleWindowScrollBy();
-  };
-
-  handleWindowScrollBy = () => {
-    setTimeout(() => {
-      window.scrollBy({
-        top: window.innerHeight - 72 + 16,
-        left: 0,
-        behavior: 'smooth',
-      });
-    }, 600);
-  };
-
   render() {
-    const { images, isLoading, error, searchImageText, totalHits } = this.state;
-    const { handleLoadMore } = this;
-    const { toggleModal } = this.props;
+    const {
+      handleLoadMore,
+      toggleModal,
+      images,
+      isLoading,
+      error,
+      searchImageText,
+      totalHits,
+    } = this.props;
 
     return (
       <>
